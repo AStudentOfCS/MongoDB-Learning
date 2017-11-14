db79cc4563894c6c109e95eaa2d4e71589e7d78e
AStudentOfCS <doduykhiem@gmail.com>
2017-11-14 13:31:05 +0700

    Add Sort, Skip, Limit Queries

--- test/reading_test.js
+++ test/reading_test.js
@@ -2,11 +2,15 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Reading users out of the database', () => {
-  let jan;
+  let jan, maria, alex, jack;
 
   beforeEach((done) => {
     jan = new User({ name: 'Jan' });
-    jan.save()
+    maria = new User({ name: 'Maria' });
+    alex = new User({ name: 'Alex' });
+    zack = new User({ name: 'Zack' });
+
+    Promise.all([jan.save(), maria.save(), alex.save(), zack.save()])
       .then(() => done());
   });
 
@@ -24,5 +28,18 @@ describe('Reading users out of the database', () => {
         assert(user.name === 'Jan');
         done();
       });
+  });
+
+  it('can skip and limit the result set', (done) => {
+    User.find({})
+      .sort({ name: 1 })
+      .skip(1)
+      .limit(2)
+      .then((users) => {
+        assert(users.length === 2);
+        assert(users[0].name === 'Jan');
+        assert(users[1].name === 'Maria');
+        done();
+      })
   });
 });
